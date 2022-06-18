612d48747b6bef4b1ce9e32be70a6486a6b0f6e1
Eric Mukuria <mukuriaeric@gmail.com>
2022-06-18 03:11:32 -0700

    commit tree 4a9d4fe53d9e9c4cc6c8e4f8ee31441233ad90a5

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let indexStart = 0; indexStart < word.length/2; indexStart++) {
+    const indexEnd = word.length - 1 -indexStart;
+    if (word[indexStart] !== word[indexEnd]) {
+      return false
+    } else {
+      return true
+    }
+    
+  }
 }
 
 /* 
   Add your pseudocode here
+  We are using a alternative approach to this challenge.
+  1. Iterate from the first character to the middle.
+     Compare the first character to the last character and so until the characters meet in the mmiddle.
+  2. Assign the last character a index and initialize the index.
+  3. If the two characters are are not strictly equal return false.
+  4. Else return true.
 */
+
+console.log(isPalindrome("power"))
 
 /*
   Add written explanation of your solution here
